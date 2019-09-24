a96ebc4c4382c7f164654399e3b839de658fed87
samir <hossnys@greenitglobe.com>
2019-09-24 11:52:40 +0200

    Update package.py

--- package.py
+++ package.py
@@ -3,7 +3,7 @@ from Jumpscale import j
 class Package(j.baseclasses.threebot_package):
     """
     to start need to run 
-    kosmos -p "j.tools.threebot_packages.get('veda_retreats',giturl='https://github.com/veda-websites/www_veda-retreats.git',branch='production')"
+    kosmos -p "j.tools.threebot_packages.get('veda_retreats',giturl='https://github.com/veda-websites/www_veda-retreats.git',branch='master')"
     kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
     """
     def _init(self, **kwargs):
